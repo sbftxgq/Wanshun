c40b8cd594fb05b437a3a4083cff0916264624db
lovechubby <lyf19820422@163.com>
2019-01-15 09:16:31 +0800

    完成入库单详情设置

---
+++ src/com/lyf/controller/QryDetailByBillNoController.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "QryDetailByBillNoController")
+public class QryDetailByBillNoController extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
---
+++ src/com/lyf/vo/IncomeViewVo.java
@@ -0,0 +1,4 @@
+package com.lyf.vo;
+
+public class IncomeViewVo {
+}
